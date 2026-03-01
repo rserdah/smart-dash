993f0341e7c8a402c82c854353669380efa97d8b
rserdah <94141015+rserdah@users.noreply.github.com>
2026-02-28 21:09:58 -0500

    Change toggle device API fn, add temperature fn
    - Renamed the toggle device API function in the frontend to 'toggleDevicePower'
    - Add set device temperature API function

--- src/api/devices.ts
+++ src/api/devices.ts
@@ -10,8 +10,22 @@ export async function fetchDevice(id: number) {
     return res.json();
 }
 
-export async function toggleDevice(id: number) {
-    const res = await fetch(`http://localhost:4000/api/devices/${id}/toggle/`, { method: 'POST' });
+export async function toggleDevicePower(id: number) {
+    const res = await fetch(`http://localhost:4000/api/devices/${id}/power/`, { method: 'PATCH' });
+
+    return res.json();
+}
+
+export async function setDeviceTemperature(id: number, temp: number) {
+    const res = await fetch(`http://localhost:4000/api/devices/${id}/temperature/`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            targetTemperature: temp
+        })
+    });
 
     return res.json();
 }
