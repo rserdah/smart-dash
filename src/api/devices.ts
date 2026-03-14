2572f749669bef955bd60c334a2df0d8495894f8
rserdah <94141015+rserdah@users.noreply.github.com>
2026-03-14 13:15:31 -0400

    Add helper function for frontend device API
    Add `setDeviceState` helper function that other set functions call.

--- src/api/devices.ts
+++ src/api/devices.ts
@@ -1,3 +1,8 @@
+import { BaseDeviceState } from '../../server/src/types/devices/types/device.types';
+import { LightState } from '../../server/src/types/devices/types/light.types';
+import { SwitchState } from '../../server/src/types/devices/types/switch.types';
+import { ClimateState } from '../../server/src/types/devices/types/climate.types';
+
 export async function fetchDevices() {
     const res = await fetch(`http://localhost:4000/api/devices/`);
     
@@ -10,30 +15,38 @@ export async function fetchDevice(id: number) {
     return res.json();
 }
 
-export async function setDevicePower(id: number, power: boolean) {
+async function setDeviceState(id: number, state: Partial<BaseDeviceState|LightState|SwitchState|ClimateState>) {
     const res = await fetch(`http://localhost:4000/api/devices/${id}/state/`, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-            power: power
-        })
+        body: JSON.stringify(state)
     });
 
     return res.json();
 }
 
-export async function setDeviceTemperature(id: number, temp: number) {
-    const res = await fetch(`http://localhost:4000/api/devices/${id}/state/`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            targetTemperature: temp
-        })
+export async function setDevicePower(id: number, power: boolean) {
+    return await setDeviceState(id, {
+        power: power
     });
+}
 
-    return res.json();
+export async function setDeviceBrightness(id: number, brightness: number) {
+    return await setDeviceState(id, {
+        brightness: brightness
+    });
+}
+
+export async function setDeviceColor(id: number, color: [number, number, number]) {
+    return await setDeviceState(id, {
+        color: color
+    });
+}
+
+export async function setDeviceTemperature(id: number, temp: number) {
+    return await setDeviceState(id, {
+        targetTemperature: temp
+    });
 }
